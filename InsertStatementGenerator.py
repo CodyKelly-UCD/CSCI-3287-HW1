84c936ad74512f8bc0d3245ae9b1af45a594bf1d
Cody Kelly <cody.kelly@ucdenver.edu>
2020-03-06 14:00:03 -0700

    Modified InsertStatementGenerator.py to generate SQL insert statements for all .csv files automatically

--- InsertStatementGenerator.py
+++ InsertStatementGenerator.py
@@ -1,42 +1,54 @@
-# This script takes a .csv file, a table name, and the number of attributes in the table
-# and generates a file containing an SQL INSERT statement with the data.
+# This script processes each .csv file in the directory it's in
+# and generates a file containing an SQL INSERT statement with the data from each file.
 
-tableName = input("\nPlease enter the table name: ")
-filePath = input("\nPlease enter the .csv file name: ")
-numOfAttributesPerRow = int(input("\nPlease enter the number of attributes per row: "))
-outputFileName = tableName+'_Insert_Statement.txt'
+from os import listdir, getcwd
+from os.path import isfile
 data = []
 
 
-# Process file and extract data
-with open(filePath if ".csv" in filePath else filePath + ".csv") as f:
+for dataFilePath in listdir(getcwd()):
+    if isfile(dataFilePath) and ".csv" in dataFilePath:
+        numOfColumns = 1
+        tableName = dataFilePath[:-4]
+        outputFileName = tableName + '_Insert_Statement.txt'
 
-    # Read data from file, strip beginning and end of whitespace, 
-    # then replace newlines with commas and split data by commas:
-    data = f.read().strip().replace("\n", ",").split(',')
-    f.close()
+        # Process file and extract data
+        with open(dataFilePath) as dataFile:
 
+            # Get the number of columns in table by counting commas in the first
+            # line of file
+            for char in dataFile.readline():
+                if char == ",":
+                    numOfColumns += 1
 
-# Write data to output file as an SQL statement
-with open(outputFileName, 'w') as f:
-    f.write('INSERT INTO {} VALUES '.format(tableName))
-    for i in range(0, len(data) - 1):
+            # Return to beginning of file
+            dataFile.seek(0)
 
-        # If this is the first attribute of a new tuple:
-        if i % numOfAttributesPerRow == 0:
-            f.write("({}, ".format(data[i]))
-
-        # If this is the last attribute of a tuple:
-        elif i % numOfAttributesPerRow == numOfAttributesPerRow - 1 % numOfAttributesPerRow:
-            f.write("{}), ".format(data[i]))
-
-        # If this is data within the middle of the tuple:
-        else:
-            f.write("{}, ".format(data[i]))
-
-    # If this is the very last attribute of the very last tuple:
-    f.write("{});".format(data[-1]))
+            # Read data from file, strip beginning and end of whitespace, 
+            # then replace newlines with commas and split data by commas:
+            data = dataFile.read().strip().replace("\n", ",").split(',')
+            dataFile.close()
 
 
-# Tell user the output file name
-print("\n\n{} has been created.\n".format(outputFileName))
+        # Write data to output file as an SQL statement
+        with open(outputFileName, 'w') as dataFile:
+            dataFile.write('INSERT INTO {} VALUES '.format(tableName))
+            for i in range(0, len(data) - 1):
+
+                # If this is the first attribute of a new tuple:
+                if i % numOfColumns == 0:
+                    dataFile.write("({}, ".format(data[i]))
+
+                # If this is the last attribute of a tuple:
+                elif i % numOfColumns == numOfColumns - 1 % numOfColumns:
+                    dataFile.write("{}), ".format(data[i]))
+
+                # If this is data within the middle of the tuple:
+                else:
+                    dataFile.write("{}, ".format(data[i]))
+
+            # If this is the very last attribute of the very last tuple:
+            dataFile.write("{});".format(data[-1]))
+        
+        # Tell user the output file name
+        print("\t{} has been created.".format(outputFileName))
